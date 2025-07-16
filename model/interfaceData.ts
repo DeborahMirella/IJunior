b905b70cdf8d91d02d235c7788508dc7f13a9cd3
DeborahMirella <deborahmirella5@gmail.com>
2025-07-16 16:48:23 -0300

    feat: Corrigindo um erro

---
+++ model/interfaceData.ts
@@ -0,0 +1,13 @@
+export interface  Data {
+
+    nome: string;
+    valor: number;
+    peso: number;
+    quantidade: number;
+
+}
+
+/*
+   Basicamente cada Item(Data) no Estoque vai ter essa 
+   forma, é bem parecido com uma struct em C   
+*/
