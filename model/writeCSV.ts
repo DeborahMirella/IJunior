10066884aa9c3fa513cf3ecbf2159e0b0054d2c0
DeborahMirella <deborahmirella5@gmail.com>
2025-07-17 14:55:19 -0300

    feat: Implementação do arquivo writeCSV.ts

--- model/writeCSV.ts
+++ model/writeCSV.ts
@@ -1,1 +1,33 @@
+/*
+  Esse módulo tem a responsabilidade de pegar um array de objetos JavaScripit
+  e escrevê-lo no arquivo estoque.csv
+*/
 
+import { createObjectCsvWriter as createCsvWriter } from "csv-writer";
+
+import { Data } from './interfaceData';
+
+const ARQUIVO_CSV = './estoque.csv'
+
+export async function escreverCSV(data: Data[]) : Promise<void> { 
+
+    const csvWriter = createCsvWriter ({
+
+        path: ARQUIVO_CSV,
+        
+        //Define as colunas do arquivo CSV
+        header: [
+
+            { id: 'id', title: 'id' },
+            { id: 'nome', title: 'id' },
+            { id: 'quantidade', title: 'quantidade '},
+            { id: 'precoUnitario', title: 'precoUnitario' },
+            { id: 'dataEntrada', title: 'dataEntrada'},
+
+        ],
+
+    });
+
+    await csvWriter.writeRecords(data);
+
+}
